8f248a560b623035439ade00c40cc56e012433c5
Bo Zhu <github@bozhu.me>
2012-12-30 17:17:33 -0500

    it's working now, but how to get rid of the error messages for aborted sockets

--- proxy.js
+++ proxy.js
@@ -12,26 +12,74 @@ http.createServer(function(client_request, client_response) {
         headers: client_request.headers
     };
     console.info(client_request.method + ' ' + client_request.url);
-    // console.log('\nClient Request:');  
-    // console.log(proxy_request_options);
 
-    var buff = [];
+    var flag = 0;
+    var proxy_request_1, proxy_request_2;
  
-    var proxy_request = http.request(proxy_request_options, function(proxy_response) {
-        proxy_response.on('data', function(chunk) {
-            buff.push(chunk);
-        });
-        proxy_response.on('end', function() {
-            var i;
-            for (i = 0; i < buff.length; i++) {
-                client_response.write(buff[i]);
+    proxy_request_1 = http.request(proxy_request_options, function(proxy_response_1) {
+        proxy_response_1.on('data', function(chunk) {
+            if (flag === 1) {
+                client_response.write(chunk);
             }
-            client_response.end();
         });
-        client_response.writeHead(proxy_response.statusCode, proxy_response.headers);
-        // console.log('\nServer Response:');
-        // console.log(proxy_response.statusCode);
-        // console.log(proxy_response.headers);
+        proxy_response_1.on('end', function() {
+            if (flag === 1) {
+                client_response.end();
+            }
+        });
+
+        proxy_request_2.abort();
+        flag = 1;
+        console.info('using 1');
+        client_response.writeHead(proxy_response_1.statusCode, proxy_response_1.headers);
     });
-    client_request.pipe(proxy_request);
+
+    proxy_request_2 = http.request(proxy_request_options, function(proxy_response_2) {
+        proxy_response_2.on('data', function(chunk) {
+            if (flag === 2) {
+                client_response.write(chunk);
+            }
+        });
+        proxy_response_2.on('end', function() {
+            if (flag === 2) {
+                client_response.end();
+            }
+        });
+
+        proxy_request_1.abort();
+        flag = 2;
+        console.info('using 2');
+        client_response.writeHead(proxy_response_2.statusCode, proxy_response_2.headers);
+    });
+
+
+    // client_request.pipe(proxy_request);
+    client_request.on('data', function(chunk) {
+        if (flag === 0) {
+            proxy_request_1.write(chunk);
+            proxy_request_2.write(chunk);
+        } else if (flag === 1) {
+            proxy_request_1.write(chunk);
+        } else if (flag === 2) {
+            proxy_request_2.write(chunk);
+        }
+    });
+    client_request.on('end', function() {
+        if (flag === 0) {
+            proxy_request_1.end();
+            proxy_request_2.end();
+        } else if (flag === 1) {
+            proxy_request_1.end();
+        } else if (flag === 2) {
+            proxy_request_2.end();
+        }
+    });
+    client_request.on('error', function(err) {
+        console.error('Server Error: ' + err.message);
+    });
 }).listen(8080);
+console.info('Listening on 8080');
+
+process.on('uncaughtException', function(err) {
+    console.error('Caught exception: ' + err);
+});
